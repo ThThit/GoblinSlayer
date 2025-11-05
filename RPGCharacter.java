30bc4dd53c6fca6b39a2efe53b98047532fd027b
TH_Thit <thuhtinthit@gmail.com>
2025-11-06 00:25:54 +0700

    Abstract Character attributes and constructors

--- RPGCharacter.java
+++ RPGCharacter.java
@@ -1,3 +1,10 @@
 public abstract class RPGCharacter {
     // abstract for character in the RPG
+    private String name;
+    private int health;
+    private String race;
+
+    public abstract void takeDamage(int dmg);
+
+    public abstract void getHeal(int heal);
 }
